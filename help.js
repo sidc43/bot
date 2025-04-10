03683997d9acad0993116fb99e0ce8e8dbeee98b
Sidharth <125322559+sidc43@users.noreply.github.com>
2025-04-09 23:59:11 -0500

    help

---
+++ help.js
@@ -0,0 +1,15 @@
+const mineflayer = require('mineflayer');
+
+function help(commands, bot) {
+  bot.on('chat', (username, message) => {
+    if (username === bot.username) return;
+
+    if (message === "help") {
+      for (const c of commands) {
+        bot.chat(c?.command + " : " + c?.desc);
+      }
+    }
+  });
+}
+
+module.exports = { help };
--- spark.js
+++ spark.js
@@ -5,14 +5,20 @@ const { registerPvp } = require('./pvp'); // PVP - JOE
 const { registerMovement } = require('./movement'); // MOVEMENT - JOE
 const { registerEventLog } = require('./eventlog'); // MEMORY - JOE
 //const { registerIdle } = require('./idle'); // AUTOPILOT - JOE
-
-
+const { help } = require('./help');
 
 const bot = mineflayer.createBot({
   host: 'localhost',
   port: 60060,
   username: 'Bot',
 });
+
+const commands = [
+  { command: "Guard me", desc: "Guards player from entities" },
+  { command: "Attack me", desc: "Starts attacking player" },
+  { command: "Follow me", desc: "Follows player indefinitely" },
+  { command: "Come to me", desc: "Bot comes to your location and sends ETA" },
+]
 
 bot.loadPlugin(pathfinder);
 
@@ -28,5 +34,6 @@ bot.on('spawn', () => {
   registerEventLog(bot);
   registerMovement(bot);
   registerPvp(bot);
+  help(commands, bot);
   //registerIdle(bot);
 });
